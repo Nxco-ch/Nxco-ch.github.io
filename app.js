5357607de960e9dec3eb3205941f89b0734ac392
Nxco-ch <nicolas.chanel33@gmail.com>
2026-05-01 00:14:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -136,3 +136,14 @@ reveals.forEach(el => {
     el.classList.add("reveal");
     observer.observe(el);
 });
+
+document.addEventListener("mousemove", (e) => {
+    const x = (e.clientX / window.innerWidth - 0.5) * 20;
+    const y = (e.clientY / window.innerHeight - 0.5) * 20;
+
+    document.body.style.backgroundPosition = `
+        ${50 + x}% ${50 + y}%,
+        ${50 - x}% ${50 - y}%,
+        center
+    `;
+});
